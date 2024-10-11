08f093d46086f4bd87f67e0e8f62f51805a155e7
Jeffrey Hui <31459252+Slijeff@users.noreply.github.com>
2024-10-11 17:31:11 -0500

    Untrack files in .gitignore

--- resources/cs511/queries/a.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    sum(p_retailprice) AS total
-FROM
-    part
-WHERE
-    p_retailprice > 1000
-    AND p_size IN (5, 10, 15, 20)
-    AND p_container <> 'JUMBO JAR'
-    AND p_mfgr LIKE 'Manufacturer#1%';
